7f1f3998da0ca0725b7356c0b7ce56caf30b1359
Noah230285 <noahjharris@outlook.com>
2024-06-08 13:56:33 +1000

    Fixed finding the package directory and added default theme

--- Editor/InputIconBinding/AddIconBindingAddressables.cs
+++ Editor/InputIconBinding/AddIconBindingAddressables.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.AddressableAssets;
-using UnityEditor.AddressableAssets.Settings;
-using UnityEngine;
-using UtilEssentials.UIToolkitUtility.Editor;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEditor.AddressableAssets;
+//using UnityEditor.AddressableAssets.Settings;
+//using UnityEngine;
+//using UtilEssentials.UIToolkitUtility.Editor;
 
-public class AddIconBindingAddressables : MonoBehaviour
-{
-    [InitializeOnLoadMethod]
-    static void Init()
-    {
-        //Find the path for this package
-        string assetPath = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
-        string beginningPath = UIToolkitUtilityFunctions.GetBeginningOfPackagePath(assetPath, "com.utility_essentials.input_binding");
+//public class AddIconBindingAddressables
+//{
+//    [InitializeOnLoadMethod]
+//    static void Init()
+//    {
+//        //Find the path for this package
+//        string assetPath = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+//        string beginningPath = UIToolkitUtilityFunctions.GetBeginningOfPackagePath(assetPath, "com.utility_essentials.input_binding");
 
-        var group = AssetDatabase.LoadAssetAtPath<AddressableAssetGroup>($"{beginningPath}/Assets/ScriptableObjects/Addressables/UXML Menu Assets.asset");
-        var settings = AddressableAssetSettingsDefaultObject.Settings;
-        if (!settings.groups.Contains(group))
-        {
-            settings.groups.Add(group);
-        }
-    }
-}
+//        var group = AssetDatabase.LoadAssetAtPath<AddressableAssetGroup>($"{beginningPath}/Assets/ScriptableObjects/Addressables/UXML Menu Assets.asset");
+//        var settings = AddressableAssetSettingsDefaultObject.Settings;
+//        if (!settings.groups.Contains(group))
+//        {
+//            settings.groups.Add(group);
+//        }
+//    }
+//}
